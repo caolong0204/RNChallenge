3f15c1088e69b2d1d4d1760c98ca25dfff355a30
Long Vũ <vucaolong0204@gmail.com>
2023-02-09 11:20:03 +0700

    feat:react-three-fiber

--- src/Container/CommonChallenges/View3D/index.tsx
+++ src/Container/CommonChallenges/View3D/index.tsx
@@ -1,22 +1,32 @@
-import * as React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber/native'
 
-import ThreeDots from '../../../Components/ThreeDot';
-
-const View3D = () => {
+function Box(props) {
+  const mesh = useRef(null)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
   return (
-    <View style={styles.container}>
-      <View style={styles.subView}>
-        <Text>3D</Text>
-        <ThreeDots />
-      </View>
-    </View>
-  );
-};
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
-export default View3D;
-
-const styles = StyleSheet.create({
-  container: {justifyContent: 'center', flex: 1, paddingHorizontal: 20},
-  subView: {flexDirection: 'row', justifyContent: 'center'},
-});
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
