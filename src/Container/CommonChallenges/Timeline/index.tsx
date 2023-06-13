2902770f4f8a1fbc68f106382659e306680e6d9a
caolong24 <129348535+caolong24@users.noreply.github.com>
2023-06-13 12:28:03 +0700

    commit time

--- src/Container/CommonChallenges/Timeline/index.tsx
+++ src/Container/CommonChallenges/Timeline/index.tsx
@@ -1,55 +1,69 @@
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
-import Svg, {Circle, Rect, SvgXml} from 'react-native-svg';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import Svg, {Rect, Text} from 'react-native-svg';
 
 const TimeLine = () => {
-  const xml = `
-  <svg width="32" height="32" viewBox="0 0 32 32">
-    <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
-      fill="url(#gradient)"
-      d="M4 0C1.79086 0 0 1.79086 0 4V28C0 30.2091 1.79086 32 4 32H28C30.2091 32 32 30.2091 32 28V4C32 1.79086 30.2091 0 28 0H4ZM17 6C17 5.44772 17.4477 5 18 5H20C20.5523 5 21 5.44772 21 6V25C21 25.5523 20.5523 26 20 26H18C17.4477 26 17 25.5523 17 25V6ZM12 11C11.4477 11 11 11.4477 11 12V25C11 25.5523 11.4477 26 12 26H14C14.5523 26 15 25.5523 15 25V12C15 11.4477 14.5523 11 14 11H12ZM6 18C5.44772 18 5 18.4477 5 19V25C5 25.5523 5.44772 26 6 26H8C8.55228 26 9 25.5523 9 25V19C9 18.4477 8.55228 18 8 18H6ZM24 14C23.4477 14 23 14.4477 23 15V25C23 25.5523 23.4477 26 24 26H26C26.5523 26 27 25.5523 27 25V15C27 14.4477 26.5523 14 26 14H24Z"
-    />
-    <defs>
-      <linearGradient
-        id="gradient"
-        x1="0"
-        y1="0"
-        x2="8.46631"
-        y2="37.3364"
-        gradient-units="userSpaceOnUse">
-        <stop offset="0" stop-color="#FEA267" />
-        <stop offset="1" stop-color="#E75A4C" />
-      </linearGradient>
-    </defs>
-  </svg>
-`;
+  const screenWidth = Dimensions.get('window').width;
+
+  const renderBars = () => {
+    const totalBars = 48;
+    const barWidth = Math.floor((screenWidth - totalBars) / totalBars);
+    const barHeight = 50;
+    const bars = [];
+
+    for (let i = 0; i < totalBars; i++) {
+      console.log('=====i', i);
+      const x = i * (barWidth + 1);
+      bars.push(
+        <Rect
+          key={i}
+          x={x}
+          y={0}
+          width={barWidth}
+          height={barHeight}
+          fill="black"
+        />,
+      );
+      if (i % 4 === 0 || i === 47) {
+        const value = i === 47 ? 0 : i / 2;
+        const positionX = getOffSetAndValue(i, barWidth, x);
+        bars.push(
+          <Text fill="#000" fontSize="11" x={positionX} y={60}>
+            {value}
+          </Text>,
+        );
+      }
+    }
+
+    return bars;
+  };
 
   return (
     <View style={[StyleSheet.absoluteFill]}>
-      <Svg height="50%" width="50%" viewBox="0 0 100 100">
-        <Circle
-          cx="50"
-          cy="50"
-          r="45"
-          stroke="blue"
-          strokeWidth="2.5"
-          fill="green"
-        />
-        <Rect
-          x="15"
-          y="15"
-          width="70"
-          height="70"
-          stroke="red"
-          strokeWidth="2"
-          fill="yellow"
-        />
+      <Svg height={100} width={screenWidth}>
+        {renderBars()}
       </Svg>
-      <SvgXml xml={xml} width="100%" height="100%" />
     </View>
   );
 };
-
+const getOffSetAndValue = (
+  index: number,
+  widthItem: number,
+  position: number,
+) => {
+  let leftOffset = -widthItem / 2 - 1;
+  if (index === 0) {
+    leftOffset = 0;
+  }
+  if (index >= 20) {
+    leftOffset = -widthItem - 1;
+  }
+  if (index >= 40) {
+    leftOffset = -widthItem - 1.5;
+  }
+  if (index === 47) {
+    leftOffset = 0.5;
+  }
+  return position + leftOffset;
+};
 export default TimeLine;
